a26cf6d988e5e0f7fb4651116839f3a0ebab60bd
victro-nuzhniy <nuzhniyva@gmail.com>
2023-07-17 22:55:50 +0300

    dev added RegisterView, added url, template

--- play/urls.py
+++ play/urls.py
@@ -2,8 +2,11 @@
 
 from django.urls import path, include
 
+from play.views import RegisterView
+
 app_name = "play"
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/registration/', RegisterView.as_view(), name='registration'),
 ]
--- play/views.py
+++ play/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""Module for class and function views 'play' app."""
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+
+class RegisterView(FormView):
+    """Class for User registration."""
+
+    form_class = UserCreationForm
+    template_name = "registration/register_form.html"
+    extra_context = {"title": "Registration"}
+    success_url = reverse_lazy("play:registration")
+
+    def form_valid(self, form: UserCreationForm) -> HttpResponseRedirect:
+        """Create new user. Add received after creating customer in Zoho CRM zoho_id."""
+        user: User = form.save()
+        login(self.request, user)
+        return super().form_valid(
+            form
+        )

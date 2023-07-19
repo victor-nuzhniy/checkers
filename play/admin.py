1117081d7e4b642ffc552c71e63a1bcea9fcac1d
victro-nuzhniy <nuzhniyva@gmail.com>
2023-07-19 11:01:51 +0300

    dev-new created admin site with Result model, created utils with active_user_found, created start.html

--- play/admin.py
+++ play/admin.py
@@ -1,3 +1,14 @@
+"""Admin site configuration for 'play' app."""
 from django.contrib import admin
 
-# Register your models here.
+from play.models import Result
+
+
+class ResultAdmin(admin.ModelAdmin):
+    """Result admin site settings."""
+
+    list_display = ("id", "player", "rival", "count")
+    list_display_links = ("id", "player")
+
+
+admin.site.register(Result, ResultAdmin)

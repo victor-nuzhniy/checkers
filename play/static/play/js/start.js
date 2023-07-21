4f4c8540d7e797b0d82b545670e6a384e455dbc1
victro-nuzhniy <nuzhniyva@gmail.com>
2023-07-21 15:17:43 +0300

    dev-new refactor sockets in start.js

--- play/static/play/js/start.js
+++ play/static/play/js/start.js
@@ -1,158 +1,203 @@
 const currentUserId = JSON.parse(document.getElementById('current_user_id').textContent);
 const currentUserName = JSON.parse(document.getElementById('current_username').textContent);
-const socket = new WebSocket(
-    "ws://"
-    + window.location.host
-    + "/ws/start/1/"
-);
-socket.onclose = function(e) {
-    console.log('Socket closed unexpectedly.');
+
+let startSocket = null
+
+function connectStartSocket() {
+    startSocket = new WebSocket(
+        "ws://"
+        + window.location.host
+        + "/ws/start/1/"
+    );
+    startSocket.onopen = function(){
+        console.log("startSocket successfully connected to the WebSocket.");
+    };
+    startSocket.onclose = function(e) {
+        console.log("WebSocket startSocket connection closed unexpectedly. Trying to reconnect in 2s...");
+        setTimeout(function() {
+            console.log("Reconnecting...");
+            connectStartSocket();
+        }, 2000);
+    };
+    startSocket.onerror = function(err) {
+        console.log("WebSocket startSocket encountered an error: " + err.message);
+        console.log("Closing the startSocket.");
+        startSocket.close();
+    };
 };
-socket.addEventListener("close", (event) => {
-    console.log("Socket was closed.");
-});
-socket.onopen = function(){
-     socket.send(JSON.stringify({
+
+connectStartSocket();
+
+startSocket.onopen = function () {
+    startSocket.send(JSON.stringify({
         'message': {
             "type": "start_refresh",
         }
     }));
+}
+
+startSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        if (data.type == "play_message") {
+                if (data.message["type"] == "start_playing"){
+            playerStatus = document.getElementById(data.message["player_pk"]);
+            rivalStatus = document.getElementById(data.message["rival_pk"]);
+            playerStatus.innerHTML = "No";
+            rivalStatus.innerHTML = "No";
+            playerStatus.removeAttribute("style");
+            rivalStatus.removeAttribute("style");
+        };
+        if (data.message["type"] == "end_playing"){
+            playerStatus = document.getElementById(data.message["winner"]);
+            rivalStatus = document.getElementById(data.message["loser"]);
+            playerStatus.innerHTML = "Yes";
+            rivalStatus.innerHTML = "Yes";
+            playerStatus.setAttribute("style", "cursor:pointer; color:blue");
+            rivalStatus.setAttribute("style", "cursor:pointer; color:blue");
+        };
+    } else {
+        console.log("Unknown message type!");
+    }
 };
-socket.onmessage = function(e) {
-    const data = JSON.parse(e.data);
-    if (data.message["type"] == "start_playing"){
-        playerStatus = document.getElementById(data.message["player_pk"]);
-        rivalStatus = document.getElementById(data.message["rival_pk"]);
-        playerStatus.innerHTML = "No";
-        rivalStatus.innerHTML = "No";
-        playerStatus.removeAttribute("style");
-        rivalStatus.removeAttribute("style");
+
+let proposeSocket = null
+
+function connectProposeSocket() {
+    proposeSocket = new WebSocket(
+        "ws://"
+        + window.location.host
+        + "/ws/propose/"
+        + currentUserId
+        + "/"
+    );
+    proposeSocket.onopen = function(){
+        console.log("proposeSocket successfully connected to the WebSocket.");
     };
-    if (data.message["type"] == "end_playing"){
-        playerStatus = document.getElementById(data.message["winner"]);
-        rivalStatus = document.getElementById(data.message["loser"]);
-        playerStatus.innerHTML = "Yes";
-        rivalStatus.innerHTML = "Yes";
-        playerStatus.setAttribute("style", "cursor:pointer; color:blue");
-        rivalStatus.setAttribute("style", "cursor:pointer; color:blue");
+    proposeSocket.onclose = function(e) {
+        console.log("WebSocket proposeSocket connection closed unexpectedly. Trying to reconnect in 2s...");
+        setTimeout(function() {
+            console.log("Reconnecting...");
+            connectProposeSocket();
+        }, 2000);
+    };
+    proposeSocket.onerror = function(err) {
+        console.log("WebSocket proposeSocket encountered an error: " + err.message);
+        console.log("Closing the proposeSocket.");
+        proposeSocket.close();
     };
 };
 
-const startSocket = new WebSocket(
-    "ws://"
-    + window.location.host
-    + "/ws/propose/"
-    + currentUserId
-    + "/"
-);
-startSocket.onclose = function(e) {
-    console.log('Socket closed unexpectedly.');
-};
-startSocket.addEventListener("close", (event) => {
-    console.log("Socket was closed.");
-});
-startSocket.onmessage = function(e) {
+connectProposeSocket();
+
+proposeSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-    if (
-        data.message["type"] == "propose_to_play"
-        && data.message["player_id"] != currentUserId
-        && data.message["rival_id"] == currentUserId
-    ) {
-        let message = document.getElementById("proposal_" + data.message["player_id"])
-        message.innerHTML =
-        '<a href="http://'
-        + window.location.host
-        + '/'
-        + data.message["player_id"]
-        + '/'
-        + currentUserId
-        + '/">'
-        + data.message["player_username"]
-        + " invite!</a>"
-        let board = document.getElementById("board")
-        board.innerHTML =
-            '<div>'
+    if (data.type == "play_message") {
+        if (
+            data.message["type"] == "propose_to_play"
+            && data.message["player_id"] != currentUserId
+            && data.message["rival_id"] == currentUserId
+        ) {
+            let message = document.getElementById("proposal_" + data.message["player_id"])
+            message.innerHTML =
+            '<a href="http://'
+            + window.location.host
+            + '/'
+            + data.message["player_id"]
+            + '/'
+            + currentUserId
+            + '/">'
             + data.message["player_username"]
-            + ' invite!'
-            + '</div>'
-            + board.innerHTML
-        message.addEventListener("click", (event) => {
-            const answerSocket = new WebSocket(
-                "ws://"
-                + window.location.host
-                + "/ws/propose/"
-                + currentUserId
-                + "/"
-            );
-            answerSocket.onclose = function(e) {
-                console.log('Socket closed unexpectedly.');
-            };
-            answerSocket.addEventListener("close", (event) => {
-                console.log("Socket was closed.");
-            });
-            answerSocket.onopen = function(){
-                answerSocket.send(JSON.stringify({
+            + " invite!</a>"
+            let board = document.getElementById("board")
+            board.innerHTML =
+                '<div>'
+                + data.message["player_username"]
+                + ' invite!'
+                + '</div>'
+                + board.innerHTML
+            message.addEventListener("click", (event) => {
+
+                proposeSocket.send(JSON.stringify({
                    'message': {
                        "type": "agree_to_play",
                        "player_id": currentUserId,
                        "player_username": currentUserName,
                        }
                 }));
-            };
-        });
+            });
+        };
+    } else {
+        console.log("Unknown message type!");
     };
 };
 
 let statuses = document.getElementsByClassName("ready-to-play");
 
+let proposeNewSocket = null
+
+function connectProposeNewSocket(userId) {
+    proposeNewSocket = new WebSocket(
+        "ws://"
+         + window.location.host
+         + "/ws/propose/"
+         + userId
+         + "/"
+    );
+    proposeNewSocket.onopen = function(){
+        console.log("proposeNewSocket successfully connected to the WebSocket.");
+    };
+    proposeNewSocket.onclose = function(e) {
+        console.log("WebSocket socket connection closed unexpectedly. Trying to reconnect in 2s...");
+        setTimeout(function() {
+            console.log("Reconnecting...");
+            connectProposeNewSocket(userId);
+        }, 2000);
+    };
+    proposeNewSocket.onerror = function(err) {
+        console.log("WebSocket proposeNewSocket encountered an error: " + err.message);
+        console.log("Closing the prososeNewSocket.");
+        proposeNewSocket.close();
+    };
+}
 
 for(let i=0; i<statuses.length; i++){
     if (currentUserId != statuses[i].id && statuses[i].innerHTML == 'Yes'){
         statuses[i].addEventListener("click", (event) => {
 
-            const proposeSocket = new WebSocket(
-                "ws://"
-                 + window.location.host
-                 + "/ws/propose/"
-                 + statuses[i].id
-                 + "/"
-                );
-            proposeSocket.onclose = function(e) {
-                console.log('Socket closed unexpectedly.');
-                };
-            proposeSocket.addEventListener("close", (event) => {
-                    console.log("Socket was closed.")
-                });
-            proposeSocket.onopen = function(){
-                proposeSocket.send(JSON.stringify({
-                    'message': {
+        connectProposeNewSocket(statuses[i].id)
+        proposeNewSocket.onopen = function() {
+            proposeNewSocket.send(JSON.stringify({
+                'message': {
                     "type": "propose_to_play",
                     "player_id": currentUserId,
                     "player_username": currentUserName,
                     "rival_id": statuses[i].id,
-                    }
-                }));
-            };
-            proposeSocket.onmessage = function(e) {
+                }
+            }));
+        };
+            proposeNewSocket.onmessage = function(e) {
                 const data = JSON.parse(e.data);
-                console.log(data.message, "outside")
-                if (
-                    data.message["type"] == "agree_to_play"
-                    && data.message["player_id"] == statuses[i].id
-                ) {
-                    let returnMessage = document.getElementById("proposal_" + data.message["player_id"])
-                    returnMessage.innerHTML =
-                    '<a href="http://'
-                    + window.location.host
-                    + '/'
-                    + currentUserId
-                    + '/'
-                    + data.message["player_id"]
-                    + '/">Join '
-                    + data.message["player_username"]
-                    + '!</a>'
-                    let board = document.getElementById("board");
-                    board.innerHTML = '<div>' + returnMessage.innerHTML + '</div>' + board.innerHTML;
+                if (data.type == 'play_message') {
+                    if (
+                        data.message["type"] == "agree_to_play"
+                        && data.message["player_id"] == statuses[i].id
+                    ) {
+                        let returnMessage = document.getElementById("proposal_" + data.message["player_id"])
+                        returnMessage.innerHTML =
+                        '<a href="http://'
+                        + window.location.host
+                        + '/'
+                        + currentUserId
+                        + '/'
+                        + data.message["player_id"]
+                        + '/">Join '
+                        + data.message["player_username"]
+                        + '!</a>'
+                        let board = document.getElementById("board");
+                        board.innerHTML = '<div>' + returnMessage.innerHTML + '</div>' + board.innerHTML;
+                    };
+                } else {
+                    console.log("Unknown message type!");
                 };
             };
         });

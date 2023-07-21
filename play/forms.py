320836f237940a7172b808bd44baf304ee5d1b81
victro-nuzhniy <nuzhniyva@gmail.com>
2023-07-22 00:19:10 +0300

    dev-new rewrite RegisterView for registration email validation option, added SignupForm

--- play/forms.py
+++ play/forms.py
@@ -1,7 +1,14 @@
 """Forms for 'play' app."""
 
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
 
 
 class UserProfileForm(forms.ModelForm):
--- play/views.py
+++ play/views.py
@@ -1,35 +1,92 @@
 """Module for class and function views 'play' app."""
 import json
 from abc import ABC
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.mail import send_mail
 from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views.generic import FormView, TemplateView
 
-from play.forms import ResultDeleteForm, UserProfileForm
+from play.forms import ResultDeleteForm, SignupForm, UserProfileForm
 from play.models import Result
-from play.utils import get_all_logged_in_users, get_all_users_data
+from play.utils import (
+    account_activation_token,
+    get_all_logged_in_users,
+    get_all_users_data,
+)
 
 
 class RegisterView(FormView):
     """Class for User registration."""
 
-    form_class = UserCreationForm
-    template_name = "registration/register_form.html"
+    form_class = SignupForm
+    template_name = "play/registration/register_form.html"
     extra_context = {"title": "Registration"}
-    success_url = reverse_lazy("play:start")
+    success_url = reverse_lazy("play:email_confirm")
 
-    def form_valid(self, form: UserCreationForm) -> HttpResponseRedirect:
+    def form_valid(self, form: SignupForm) -> HttpResponseRedirect:
         """Create new user. Add received after creating customer in Zoho CRM zoho_id."""
-        user: User = form.save()
-        login(self.request, user)
+        user: User = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        current_site = get_current_site(self.request)
+        mail_subject: str = "Activate your account"
+        message: str = render_to_string(
+            "play/registration/email_template.html",
+            {
+                "user": user,
+                "domain": current_site.domain,
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "token": account_activation_token.make_token(user),
+            },
+        )
+        to_email: str = form.cleaned_data.get("email")
+        send_mail(mail_subject, message, "admin@email.com", [to_email])
         return super().form_valid(form)
+
+
+class RegisterEmailConfirmView(TemplateView):
+    """Class view for redirection after registration."""
+
+    template_name = "play/registration/email_confirm.html"
+    extra_context = {"title": "Email confirmation"}
+
+
+class EmailActivationView(TemplateView):
+    """Class view for getting activation info."""
+
+    template_name = "play/registration/email_activation_result.html"
+    extra_context = {"title": "Email activation"}
+
+    def get_context_data(self, **kwargs: Any) -> Dict:
+        """Get context data for the view."""
+        context = super().get_context_data(**kwargs)
+        try:
+            uid = force_str(urlsafe_base64_decode(self.kwargs.get("uidb64")))
+            user: Optional[User] = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, ObjectDoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(
+            user, self.kwargs.get("token")
+        ):
+            user.is_active = True
+            user.save()
+            message = (
+                "Thank you for your email confirmation. Now you can login your account."
+            )
+        else:
+            message = "Activation link is not valid!"
+        context["message"] = message
+        return context
 
 
 class MainView(UserPassesTestMixin, TemplateView, ABC):
@@ -85,7 +142,9 @@ class RatingView(TemplateView):
         context: Dict = super().get_context_data(**kwargs)
         context["players"] = get_all_users_data().order_by("id")
         context["players_points"] = get_all_users_data().order_by("points").reverse()
-        context["players_plays"] = get_all_users_data().order_by("plays_number").reverse()
+        context["players_plays"] = (
+            get_all_users_data().order_by("plays_number").reverse()
+        )
         return context
 
 
@@ -104,9 +163,9 @@ class ProfileView(UserPassesTestMixin, TemplateView):
     def get_context_data(self, **kwargs: Any) -> Dict:
         """Get context data for the view."""
         context: Dict = super().get_context_data(**kwargs)
-        statistics: QuerySet = get_all_logged_in_users().filter(
-            id=self.kwargs["pk"]
-        ).first()
+        statistics: QuerySet = (
+            get_all_logged_in_users().filter(id=self.kwargs["pk"]).first()
+        )
         results: QuerySet = Result.objects.filter(player=self.kwargs["pk"]).reverse()
         context.update(
             {

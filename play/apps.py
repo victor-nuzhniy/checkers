46a7f4677f0705940231a3a9c58354805bd996fc
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-11 19:43:51 +0200

    latest_formatters format play apps

--- play/apps.py
+++ play/apps.py
@@ -6,5 +6,5 @@ from django.apps import AppConfig
 class PlayConfig(AppConfig):
     """Class with configuration for 'play' app."""
 
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "play"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'play'

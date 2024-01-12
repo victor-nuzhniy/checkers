08b26e6f30ddef0bb1e67bd418f193c86adc1ae0
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-12 17:08:48 +0200

    latest_formatters format play routing

--- play/routing.py
+++ play/routing.py
@@ -2,15 +2,15 @@
 
 from django.urls import re_path
 
-from . import consumers
+from play import consumers
 
 websocket_urlpatterns = [
     re_path(
-        r"ws/play/(?P<player_id>\w+)/(?P<rival_id>\w+)/$",
+        r'ws/play/(?P<player_id>\w+)/(?P<rival_id>\w+)/$',
         consumers.PlayConsumer.as_asgi(),
     ),
     re_path(
-        r"ws/start/(?P<player_id>\w+)/$",
+        r'ws/start/(?P<player_id>\w+)/$',
         consumers.StartConsumer.as_asgi(),
     ),
 ]

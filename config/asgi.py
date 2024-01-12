4d1a78f7220fc21996469823766182add9c06735
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-12 17:11:40 +0200

    latest_formatters format config asgi

--- config/asgi.py
+++ config/asgi.py
@@ -14,18 +14,16 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django_asgi_app = get_asgi_application()
 
-import play.routing
-
+from play import routing  # noqa #E402
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(play.routing.websocket_urlpatterns))
+        'http': django_asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
         ),
-    }
+    },
 )

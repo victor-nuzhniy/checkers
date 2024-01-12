14756c668a40bbef5f08f1dda72014ad3eff6eba
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-12 17:17:06 +0200

    latest_formatters format deploy_config settings

--- deploy_config/settings.py
+++ deploy_config/settings.py
@@ -1,4 +1,4 @@
 """Settings for checkers project with daphne production routing."""
-from config.settings import *  # noqa: F401,F403
+from config.settings import *  # noqa: F401,F403,WPS347
 
-ASGI_APPLICATION = "deploy_config.asgi.application"
+ASGI_APPLICATION = 'deploy_config.asgi.application'

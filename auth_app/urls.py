f48bfa8fcd881390a156a73266cb8b5e646f746f
victro-nuzhniy <nuzhniyva@gmail.com>
2024-01-15 12:22:16 +0200

    master changed app_name and template storage in html files and urls.py

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -13,51 +13,55 @@ app_name = 'auth_app'
 
 urlpatterns = [
     path(
-        'accounts/login/',
+        'login/',
         views.CustomLoginView.as_view(),
         name='login',
     ),
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path(
-        'accounts/password_change/',
+        'password_change/',
         views.CustomPasswordChangeView.as_view(),
         name='password_change',
     ),
     path(
-        'accounts/password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='play/password_change_done.html'),
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='auth_app/password_change_done.html',
+        ),
         name='password_change_done',
     ),
     path(
-        'accounts/password_reset/',
+        'password_reset/',
         views.CustomPasswordResetView.as_view(),
         name='password_reset',
     ),
     path(
-        'accounts/password_reset/done',
-        PasswordResetDoneView.as_view(template_name='play/password_reset_done.html'),
+        'password_reset/done',
+        PasswordResetDoneView.as_view(
+            template_name='auth_app/password_reset_done.html',
+        ),
         name='password_reset_done',
     ),
     path(
-        'accounts/reset/<uidb64>/<token>/',
+        'reset/<uidb64>/<token>/',
         views.CustomPasswordResetConfirmView.as_view(),
         name='password_reset_confirm',
     ),
     path(
-        'accounts/reset/done',
+        'reset/done',
         PasswordResetCompleteView.as_view(
-            template_name='play/password_reset_complete.html',
+            template_name='auth_app/password_reset_complete.html',
         ),
         name='password_reset_complete',
     ),
-    path('accounts/registration/', views.RegisterView.as_view(), name='registration'),
+    path('registration/', views.RegisterView.as_view(), name='registration'),
     path(
-        'accounts/email_verification/<uidb64>/<token>',
+        'email_verification/<uidb64>/<token>',
         views.EmailActivationView.as_view(),
         name='activate',
     ),
     path(
-        'accounts/email_confirm/',
+        'email_confirm/',
         views.RegisterEmailConfirmView.as_view(),
         name='email_confirm',
     ),
